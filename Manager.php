2bb914208eadb6f0e48380389a0773ea4acb99df
SeynvoAM <sejnovalexey@gmail.com>
2014-06-13 17:22:03 +0300

    Init commit.
    Signed-off-by: SeynvoAM <sejnovalexey@gmail.com>

---
+++ Manager.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace flexibuild\FileSystem;
+
+/**
+ * flexibuild\FileSystem\Manager is a Yii2 component class that helps to upload and manage files.
+ *
+ * @author SeynvoAM <sejnovalexey@gmail.com>
+ */
+class Manager
+{
+    //put your code here
+}
