b89c65b78176c68e75acb8742ca517bc6f273a67
SeynovAM <sejnovalexey@gmail.com>
2014-11-20 17:03:39 +0300

    Init commit of image/ResizeFormatter.php (WIP)
    Signed-off-by: SeynovAM <sejnovalexey@gmail.com>

---
+++ formatters/image/ResizeFormatter.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace flexibuild\file\formatters\image;
+
+use flexibuild\file\formatters\Formatter;
+
+/**
+ * Formatter that resizes image file.
+ *
+ * @author SeynovAM <sejnovalexey@gmail.com>
+ */
+class ResizeFormatter extends Formatter
+{
+    /**
+     * @inheritdoc
+     */
+    public function format($readFilePath)
+    {
+        
+    }
+}
