54ef6a6bf78a2108cfb41e751a12cabca1601aa1
SeynovAM <sejnovalexey@gmail.com>
2015-01-05 18:29:27 +0300

    Added BlueimpJQueryUploader (WIP).
    Signed-off-by: SeynovAM <sejnovalexey@gmail.com>

---
+++ widgets/BlueimpJQueryUploader.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace flexibuild\file\widgets;
+
+use yii\widgets\InputWidget;
+
+/**
+ * Widget that will be rendered as bluimp jQuery ajax file uploader.
+ *
+ * @author SeynovAM <sejnovalexey@gmail.com>
+ */
+class BlueimpJQueryUploader extends InputWidget
+{
+    // @todo dev this widget
+}
